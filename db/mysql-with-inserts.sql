62db99252e433ca03bd9a59c18788cd1645e786d
Miguel Reboiro-Jato <mrjato@uvigo.es>
2016-02-29 19:01:59 +0100

    Fixes column nullability in MySQL scripts
    Some columns of the MySQL database created by the scripts in the "db"
    directory where incorrectly set as nullable. This commit fixes this
    error and sets them as non nullable columns.

--- db/mysql-with-inserts.sql
+++ db/mysql-with-inserts.sql
@@ -2,14 +2,14 @@ CREATE DATABASE `daaexample`;
 
 CREATE TABLE `daaexample`.`people` (
 	`id` int NOT NULL AUTO_INCREMENT,
-	`name` varchar(50) DEFAULT NULL,
-	`surname` varchar(100) DEFAULT NULL,
+	`name` varchar(50) NOT NULL,
+	`surname` varchar(100) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `daaexample`.`users` (
 	`login` varchar(100) NOT NULL,
-	`password` varchar(64) DEFAULT NULL,
+	`password` varchar(64) NOT NULL,
 	PRIMARY KEY (`login`)
 );
 
