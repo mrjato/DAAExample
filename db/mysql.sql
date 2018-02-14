a18e576e3f1625f010e6b383df1c5d04b29df739
Miguel Reboiro-Jato <mrjato@uvigo.es>
2018-02-14 20:09:59 +0100

    Sets UTF-8 encoding for the database tables
    Database encoding has been changed to UTF-8. This should solve character
    presentation errors in Windows environments.

--- db/mysql.sql
+++ db/mysql.sql
@@ -5,12 +5,12 @@ CREATE TABLE `daaexample`.`people` (
 	`name` varchar(50) NOT NULL,
 	`surname` varchar(100) NOT NULL,
 	PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `daaexample`.`users` (
 	`login` varchar(100) NOT NULL,
 	`password` varchar(64) NOT NULL,
 	PRIMARY KEY (`login`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 GRANT ALL ON `daaexample`.* TO 'daa'@'localhost' IDENTIFIED BY 'daa';
