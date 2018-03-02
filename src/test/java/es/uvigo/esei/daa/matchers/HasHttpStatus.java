0ee6f4faed75f0b3554845c647d92ca1ff0e57c8
Miguel Reboiro-Jato <mrjato@uvigo.es>
2018-03-02 11:34:05 +0100

    Modifies HasHttpStatus matcher for a more descriptive message
    The message showed currently by the HasHttpStatus when de expected
    status didn't match the actual status was not very informative, as
    the actual status was not shown in the message.
    
    This commit modifies this behaviour to show both status (expected and
    actual) in the message.

--- src/test/java/es/uvigo/esei/daa/matchers/HasHttpStatus.java
+++ src/test/java/es/uvigo/esei/daa/matchers/HasHttpStatus.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -34,11 +34,11 @@ import org.hamcrest.TypeSafeMatcher;
 
 public class HasHttpStatus extends TypeSafeMatcher<Response> {
 	private final StatusType expectedStatus;
-	
+
 	public HasHttpStatus(StatusType expectedStatus) {
 		this.expectedStatus = requireNonNull(expectedStatus);
 	}
-	
+
 	public HasHttpStatus(int expectedStatus) {
 		this(Status.fromStatusCode(expectedStatus));
 	}
@@ -49,40 +49,45 @@ public class HasHttpStatus extends TypeSafeMatcher<Response> {
 	}
 
 	@Override
+	protected void describeMismatchSafely(Response item, Description mismatchDescription) {
+		mismatchDescription.appendText("was ").appendValue(item.getStatusInfo());
+	}
+
+	@Override
 	protected boolean matchesSafely(Response item) {
 		return item != null && expectedStatus.getStatusCode() == item.getStatusInfo().getStatusCode();
 	}
-	
+
 	@Factory
 	public static Matcher<Response> hasHttpStatus(StatusType expectedStatus) {
 		return new HasHttpStatus(expectedStatus);
 	}
-	
+
 	@Factory
 	public static Matcher<Response> hasHttpStatus(int expectedStatus) {
 		return new HasHttpStatus(expectedStatus);
 	}
-	
+
 	@Factory
 	public static Matcher<Response> hasOkStatus() {
 		return new HasHttpStatus(Response.Status.OK);
 	}
-	
+
 	@Factory
 	public static Matcher<Response> hasBadRequestStatus() {
 		return new HasHttpStatus(Response.Status.BAD_REQUEST);
 	}
-	
+
 	@Factory
 	public static Matcher<Response> hasInternalServerErrorStatus() {
 		return new HasHttpStatus(Response.Status.INTERNAL_SERVER_ERROR);
 	}
-	
+
 	@Factory
 	public static Matcher<Response> hasUnauthorized() {
 		return new HasHttpStatus(Response.Status.UNAUTHORIZED);
 	}
-	
+
 	@Factory
 	public static Matcher<Response> hasForbidden() {
 		return new HasHttpStatus(Response.Status.FORBIDDEN);
